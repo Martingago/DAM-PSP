366859240057d532b4a1316224f2c471f472e5ea
martin <gagochorenmartin@gmail.com>
2023-11-10 13:28:29 +0000

    continuacion ejercicio 3

--- UD_02/Exercicios_con_threads/src/Exercicio3/MainEjercicio3.java
+++ UD_02/Exercicios_con_threads/src/Exercicio3/MainEjercicio3.java
@@ -1,9 +1,25 @@
 package Exercicio3;
 
-
 public class MainEjercicio3 {
 
     public static void main(String[] args) {
+
+        //Código para probar que el programa funciona sin tener que pasar parámetros:
+        String[] argsTest = {"test.txt", "test2.txt", "test3.txt", "test4.txt", "test5.txt", "test6.txt"};
+        long startTime = System.currentTimeMillis(); // Tiempo inicial
+        ThreadManager thread = new ThreadManager();
+
+        //Ejecuta los threads de forma secuencial
+        thread.ejecutarThreadsSecuenciales(argsTest);
+        
+        //Ejecuta los thread de forma paralela
+        //thread.ejecutarThreadsParalelos(argsTest);
+        
+        long endTime = System.currentTimeMillis(); // Tiempo final
+        long executionTime = endTime - startTime; // Tiempo de ejecución
+
+        System.out.println("------------------------------------------------------------------\n"
+                          + "El tiempo de ejecución fue de: " + executionTime + " milisegundos.");
 
     }
 
--- UD_02/Exercicios_con_threads/src/Exercicio3/ReadDataFromDocument.java
+++ UD_02/Exercicios_con_threads/src/Exercicio3/ReadDataFromDocument.java
@@ -1,13 +1,51 @@
-
 package Exercicio3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadDataFromDocument {
 
-    public int readCharsFromDocument(String path){
-    try{
-    
-    }catch(){}
-    return 2;
+    /**
+     * Función que, pasado un documento, muestra las características del mismo
+     * @param path dirección en la que se encuentra el documento a analizar
+     * @return int[] con la información del archivo ordenada por:
+     * 0 => N.- caracteres
+     * 1 => N.- palabras
+     * 3 => N.- líneas
+     */
+    public int[] readCharacteristicsFromDocument(String path) {
+        int caracteres = 0; //total de carácteres (incluídos espacios)
+        int palabras = 0; //total de  palabras
+        int lineas = 0; //total de líneas
+
+        try {
+            File fichero = new File(path);
+            FileReader fr = new FileReader(fichero);
+            BufferedReader br = new BufferedReader(fr);
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                caracteres += line.length();
+                lineas++;
+                String[] words = line.split("\\s+"); //separa los elementos de la línea por " " y los introduce en un array
+                palabras += words.length;
+            }
+            System.out.println("------------------------------------\n"
+                    + "Las características del archivo son:\n"
+                    + "Caracteres: " + caracteres + "\n"
+                    + "Palabras:  " + palabras + "\n"
+                    + "Líneas: " + lineas + "\n"
+                    + "------------------------------------");
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se ha encontrado el fichero especificado" + ex);
+        } catch (IOException ex) {
+            System.out.println("Error de excepción al leer el fichero:" + ex);
+        }
+        int[] resultado = {caracteres, palabras, lineas};
+        return resultado;
     }
-    
+
 }
