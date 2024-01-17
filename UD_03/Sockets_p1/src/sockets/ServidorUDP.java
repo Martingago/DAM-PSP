2db2d1dbcdf0bc5e05ce604d16c2a7f42330a124
Martin <gagochorenmartin@gmail.com>
2024-01-17 13:40:42 +0100

    ejercicio 1 ud 3

--- UD_03/Sockets_p1/src/sockets/ServidorUDP.java
+++ UD_03/Sockets_p1/src/sockets/ServidorUDP.java
@@ -6,40 +6,52 @@ import java.net.*;
 public class ServidorUDP {
 
     private static int PUERTO_LOCAL;
-
     //Se inicializa el servidor
-    public static void main(String[] args) {
-
+    public DatagramSocket initServidorUDP(int puerto) {
+        DatagramSocket socket = null;
         try {
-
-            PUERTO_LOCAL = 12345;
-            DatagramSocket mySocket = new DatagramSocket(PUERTO_LOCAL);
-            //buffer de lectura de datos:
-            byte[] buffer = new byte[128];
-
-            System.out.println("Servidor a la escucha.\nPuerto nº: " + PUERTO_LOCAL + "\n");
-            //Paquete con la información recibida:
-
-            DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
-            mySocket.receive(paquete);
-
-            System.out.println("-------------------------------------------------------");
-            System.out.println("| Mensaje recibido en el servidor                     |");
-            System.out.println("-------------------------------------------------------");
-            System.out.printf("| Ip de origen: %-37s |\n", paquete.getAddress().getHostAddress());
-            System.out.printf("| Puerto de origen: %-33s |\n", paquete.getPort());
-            System.out.printf("| Puerto de destino: %-32s |\n", PUERTO_LOCAL);
-            System.out.printf("| Numero de caracteres recibidos: %-19s |\n", paquete.getLength());
-            System.out.println("| Mensaje recibido:                                   |");
-            System.out.println(new String(paquete.getData()));
-            System.out.println("-------------------------------------------------------");
-            System.out.println("Cerrando socket servidor");
-            mySocket.close();
+            PUERTO_LOCAL = puerto;
+            socket = new DatagramSocket(PUERTO_LOCAL);
+            System.out.println("Servidor lanzado con éxito!");
         } catch (SocketException ex) {
             System.out.println("Error en el proceso del socket \n" + ex);
-        } catch (IOException ex) {
-            System.out.println("error de interrupción del sistema \n" + ex);
         }
-
+        System.out.println("continuamos...");
+        return socket;
     }
+
+    /**
+     * Funcion que mantiene a la escucha a un socket
+     * @param socket
+     * @return String de salida con los datos recibidos / o de finalización de la escucha
+     */
+    public String listenServidorUDP(DatagramSocket socket) {
+        String salida = null;
+        try {
+            System.out.println("Comienza la escucha!");
+            //buffer de lectura de datos:
+            byte[] buffer = new byte[128];
+            DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
+            socket.receive(paquete);
+            String mensaje = new String(paquete.getData());
+            //Se cierra el socket
+            socket.close();
+            salida = "DATOS RECIBIDOS EN EL SERVIDOR: "+
+                    "\nIP de origen: " + paquete.getAddress().getHostAddress() +
+                    "\nPuerto de origen: " + paquete.getPort() +
+                    "\nPuerto de destino: " + PUERTO_LOCAL +
+                    "\nNumero de caracteres recibidos: " + paquete.getLength() +
+                    "\nMensaje recibido: \n"+ mensaje;
+            System.out.println("Servidor cerrado");
+        } catch (IOException ex) {
+            System.out.println("Error de excepcion escucha del servidor: " + ex);
+            socket.close();
+            salida = "Ha finalizado la ejecución del servidor.";
+            
+        }finally{
+            System.out.println("Terminada la escucha del servidor");
+        }
+        return salida;
+    }
+
 }
